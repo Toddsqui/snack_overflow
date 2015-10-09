045d6a96ee38330f4794c268e28b1ba1ff99a7bd
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-10-09 20:12:10 +0900

    Generated an answers migration and model and added the title string answer and made the variable belong to one user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008140939) do
+ActiveRecord::Schema.define(version: 20151009110900) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
