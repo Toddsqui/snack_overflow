18cf6d6af3e2ad2bde3a8879233a4f078bb5b51b
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-10-22 23:21:12 +0900

    We manually edited the create answers table and then we ran the rake db migrate command in order to incorporate the changes into tables.

--- db/migrate/20151009110900_create_answers.rb
+++ db/migrate/20151009110900_create_answers.rb
@@ -1,10 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.string :title
       t.text :description
 
       t.belongs_to :user
+      t.belongs_to :question
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@
 ActiveRecord::Schema.define(version: 20151009110900) do
 
   create_table "answers", force: :cascade do |t|
-    t.string   "title"
     t.text     "description"
     t.integer  "user_id"
+    t.integer  "question_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
