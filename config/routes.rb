266f2baa3ac752a58c7cf891ede4b3b65007c1f3
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-11-02 21:59:09 +0900

    Edited the routes rb file from the card that you provided.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "home#index"
-  get 'single_question/index' => 'single_question#index'
-  get 'new_question/index' => 'new_question#index'
+  get 'single_question/new', to: 'question#new', as: 'new_question'
+  post 'single_question/create', to: 'single_question#create', as: 'create_question'
+  get 'single_question/:id', to: 'single_question#show', as: 'question'
 end
